3ac97e0e13a24c8c554fea328d59c57fcda98270
Hibaboudiaj <boudiajhiba2@gmail.com>
2026-09-03 16:49:38 +0100

    feat: add categories section with responsive cards

--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -2,11 +2,14 @@ import Hero from "../components/common/Hero";
 
 import About from "../components/common/About";
 
+import CategoriesSection from "../components/common/CategoriesSection";
+
 function Home() {
   return (
     <div>
       <Hero />
       <About />
+      <CategoriesSection />
     </div>
   );
 }
